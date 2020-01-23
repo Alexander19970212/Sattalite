0d60b7636578d0786b783892fd449b20f14f7e00
Alexander19970212 <53803031+alexander19970212@users.noreply.github.com>
2020-01-23 13:29:51 +1000

    Writed class. Corrected optimization

--- test_2.py
+++ test_2.py
@@ -26,6 +26,8 @@ class Optimization:
         m = 3
         self.num_iter = 0
 
+        self.current_body = ''
+
         self.list_bodies['module_1'] = m * np.array([-2, 4, -6, 2, -2, 2, 100])
         self.list_bodies['module_2'] = m * np.array([-2, 2, -4, 4, -2, 2, 150])
         self.list_bodies['module_3'] = m * np.array([-4, 4, -1, 2, -4, 4, 200])
@@ -36,6 +38,15 @@ class Optimization:
         for name in self.list_bodies:
             # print(np.delete(list_bodies[name], [6]).reshape((3, 2)))
             self.creation_workspace(name, np.delete(self.list_bodies[name], [6]).reshape((3, 2)))
+
+        self.test = Inter(self.bodies_test, self.bodies_test.keys())
+        self.walls_function()
+        self.restricted_area()
+
+        for name in self.walls:
+            self.test.filling(name, [0, 0, 0, 0, 0, 0])
+        for name in self.bans:
+            self.test.filling(name, [0, 0, 0, 0, 0, 0])
 
     '''for name in list_bodies_r:
         verts[name] = np.array([[[list_bodies_r[name][0, 0], list_bodies_r[name][1, 0], list_bodies_r[name][2, 0]],
@@ -105,177 +116,178 @@ class Optimization:
     def restricted_area(self):
 
         self.bans['ban_1'] = np.array(
-            [[-self.px / 2 - self.d / 2 - self.d_ban, -self.px / 2 - self.d / 2], [py / 2 + d / 2, py / 2 + d / 2 + d_ban], [-pz / 2, pz / 2]])
-        bans['ban_2'] = np.array(
-            [[-px / 2 - d / 2 - d_ban, -px / 2 - d / 2], [-py / 2 - d / 2 - d_ban, -py / 2 - d / 2], [-pz / 2, pz / 2]])
-        bans['ban_3'] = np.array(
-            [[px / 2 + d / 2, px / 2 + d / 2 + d_ban], [py / 2 + d / 2, py / 2 + d / 2 + d_ban], [-pz / 2, pz / 2]])
-        bans['ban_4'] = np.array(
-            [[px / 2 + d / 2, px / 2 + d / 2 + d_ban], [-py / 2 - d / 2 - d_ban, -py / 2 - d / 2], [-pz / 2, pz / 2]])
+            [[-self.px / 2 - self.d / 2 - self.d_ban, -self.px / 2 - self.d / 2], [self.py / 2 + self.d / 2, self.py / 2 + self.d / 2 + self.d_ban], [-self.pz / 2, self.pz / 2]])
+        self.bans['ban_2'] = np.array(
+            [[-self.px / 2 - self.d / 2 - self.d_ban, -self.px / 2 - self.d / 2], [-self.py / 2 - self.d / 2 - self.d_ban, -self.py / 2 - self.d / 2], [-self.pz / 2, self.pz / 2]])
+        self.bans['ban_3'] = np.array(
+            [[self.px / 2 + self.d / 2, self.px / 2 + self.d / 2 + self.d_ban], [self.py / 2 + self.d / 2, self.py / 2 + self.d / 2 + self.d_ban], [-self.pz / 2, self.pz / 2]])
+        self.bans['ban_4'] = np.array(
+            [[self.px / 2 + self.d / 2, self.px / 2 + self.d / 2 + self.d_ban], [-self.py / 2 - self.d / 2 - self.d_ban, -self.py / 2 - self.d / 2], [-self.pz / 2, self.pz / 2]])
 
-        for name in bans:
-            creation_workspace(name=name, Shape_body=bans[name])
+        for name in self.bans:
+            self.creation_workspace(name=name, Shape_body=self.bans[name])
 
         '''for name in bans:
             test.filling(name, [0, 0, 0, 0, 0, 0])'''
 
-    def change_position(name, number_wall, pos_1, pos_2, pos_3):
-        history_args[name] = np.array([number_wall, pos_1, pos_2, pos_3])
+    def change_position(self, name, number_wall, pos_1, pos_2, pos_3):
+        self.history_args[name] = np.array([number_wall, pos_1, pos_2, pos_3])
         number_wall = int(number_wall % 7)
 
         pos_3 = int(pos_3 // 3) * 90
         if number_wall == 0:
             alpha = 90
-            offset_y = py / 2 - d / 2 + list_bodies[name][4]
+            offset_y = self.py / 2 - self.d / 2 + self.list_bodies[name][4]
             beta = pos_3
-            if pos_1 >= px / 2: offset_x = px / 2
-            if pos_1 <= -px / 2:
-                offset_x = -px / 2
+            if pos_1 >= self.px / 2: offset_x = self.px / 2
+            elif pos_1 <= -self.px / 2:
+                offset_x = -self.px / 2
             else:
                 offset_x = pos_1
 
-            if pos_2 >= pz / 2: offset_z = pz / 2
-            if pos_2 <= -pz / 2:
-                offset_z = -pz / 2
+            if pos_2 >= self.pz / 2: offset_z = self.pz / 2
+            elif pos_2 <= -self.pz / 2:
+                offset_z = -self.pz / 2
             else:
                 offset_z = pos_2
             gamma = 0
 
         if number_wall == 1:
             alpha = -90
-            offset_y = py / 2 + d / 2 - list_bodies[name][4]
+            offset_y = self.py / 2 + self.d / 2 - self.list_bodies[name][4]
             beta = pos_3
-            if pos_1 >= px / 2: offset_x = px / 2
-            if pos_1 <= -px / 2:
-                offset_x = -px / 2
+            if pos_1 >= self.px / 2: offset_x = self.px / 2
+            elif pos_1 <= -self.px / 2:
+                offset_x = -self.px / 2
             else:
                 offset_x = pos_1
 
-            if pos_2 >= pz / 2: offset_z = pz / 2
-            if pos_2 <= -pz / 2:
-                offset_z = -pz / 2
+            if pos_2 >= self.pz / 2: offset_z = self.pz / 2
+            elif pos_2 <= -self.pz / 2:
+                offset_z = -self.pz / 2
             else:
                 offset_z = pos_2
             gamma = 0
 
         if number_wall == 2:
             beta = -90
-            offset_x = px / 2 - d / 2 + list_bodies[name][4]
+            offset_x = self.px / 2 - self.d / 2 + self.list_bodies[name][4]
             alpha = pos_3
-            if pos_1 >= py / 2: offset_y = py / 2
-            if pos_1 <= -py / 2:
-                offset_y = -py / 2
+            if pos_1 >= self.py / 2: offset_y = self.py / 2
+            elif pos_1 <= -self.py / 2:
+                offset_y = -self.py / 2
             else:
                 offset_y = pos_1
 
-            if pos_2 >= pz / 2: offset_z = pz / 2
-            if pos_2 <= -pz / 2:
-                offset_z = -pz / 2
+            if pos_2 >= self.pz / 2: offset_z = self.pz / 2
+            elif pos_2 <= -self.pz / 2:
+                offset_z = -self.pz / 2
             else:
                 offset_z = pos_2
             gamma = 0
 
         if number_wall == 3:
             beta = 90
-            offset_x = px / 2 + d / 2 - list_bodies[name][4]
+            offset_x = self.px / 2 + self.d / 2 - self.list_bodies[name][4]
             alpha = pos_3
-            if pos_1 >= py / 2: offset_y = py / 2
-            if pos_1 <= -py / 2:
-                offset_y = -py / 2
+            if pos_1 >= self.py / 2: offset_y = self.py / 2
+            elif pos_1 <= -self.py / 2:
+                offset_y = -self.py / 2
             else:
                 offset_y = pos_1
 
-            if pos_2 >= pz / 2: offset_z = pz / 2
-            if pos_2 <= -pz / 2:
-                offset_z = -pz / 2
+            if pos_2 >= self.pz / 2: offset_z = self.pz / 2
+            elif pos_2 <= -self.pz / 2:
+                offset_z = -self.pz / 2
             else:
                 offset_z = pos_2
             gamma = 0
 
         if number_wall == 4:
             alpha = -90
-            offset_y = -(py / 2 - d / 2 + list_bodies[name][4])
+            offset_y = -(self.py / 2 - self.d / 2 + self.list_bodies[name][4])
             beta = pos_3
-            if pos_1 >= px / 2: offset_x = px / 2
-            if pos_1 <= -px / 2:
-                offset_x = -px / 2
+            if pos_1 >= self.px / 2: offset_x = self.px / 2
+            elif pos_1 <= -self.px / 2:
+                offset_x = -self.px / 2
             else:
                 offset_x = pos_1
 
-            if pos_2 >= pz / 2: offset_z = pz / 2
-            if pos_2 <= -pz / 2:
-                offset_z = -pz / 2
+            if pos_2 >= self.pz / 2: offset_z = self.pz / 2
+            elif pos_2 <= -self.pz / 2:
+                offset_z = -self.pz / 2
             else:
                 offset_z = pos_2
             gamma = 0
 
         if number_wall == 5:
             alpha = 90
-            offset_y = -(py / 2 + d / 2 - list_bodies[name][4])
+            offset_y = -(self.py / 2 + self.d / 2 - self.list_bodies[name][4])
             beta = pos_3
-            if pos_1 >= px / 2: offset_x = px / 2
-            if pos_1 <= -px / 2:
-                offset_x = -px / 2
+            if pos_1 >= self.px / 2: offset_x = self.px / 2
+            elif pos_1 <= -self.px / 2:
+                offset_x = -self.px / 2
             else:
                 offset_x = pos_1
 
-            if pos_2 >= pz / 2: offset_z = pz / 2
-            if pos_2 <= -pz / 2:
-                offset_z = -pz / 2
+            if pos_2 >= self.pz / 2: offset_z = self.pz / 2
+            elif pos_2 <= -self.pz / 2:
+                offset_z = -self.pz / 2
             else:
                 offset_z = pos_2
             gamma = 0
 
         if number_wall == 6:
             beta = 90
-            offset_x = -(px / 2 - d / 2 + list_bodies[name][4])
+            offset_x = -(self.px / 2 - self.d / 2 + self.list_bodies[name][4])
             alpha = pos_3
-            if pos_1 >= py / 2: offset_y = py / 2
-            if pos_1 <= -py / 2:
-                offset_y = -py / 2
+            if pos_1 >= self.py / 2: offset_y = self.py / 2
+            elif pos_1 <= -self.py / 2:
+                offset_y = -self.py / 2
             else:
                 offset_y = pos_1
 
-            if pos_2 >= pz / 2: offset_z = pz / 2
-            if pos_2 <= -pz / 2:
-                offset_z = -pz / 2
+            if pos_2 >= self.pz / 2: offset_z = self.pz / 2
+            elif pos_2 <= -self.pz / 2:
+                offset_z = -self.pz / 2
             else:
                 offset_z = pos_2
             gamma = 0
 
         if number_wall == 7:
             beta = -90
-            offset_x = -(px / 2 + d / 2 - list_bodies[name][4])
+            offset_x = -(self.px / 2 + self.d / 2 - self.list_bodies[name][4])
             alpha = pos_3
-            if pos_1 >= py / 2: offset_y = py / 2
-            if pos_1 <= -py / 2:
-                offset_y = -py / 2
+            if pos_1 >= self.py / 2: offset_y = self.py / 2
+            elif pos_1 <= -self.py / 2:
+                offset_y = -self.py / 2
             else:
                 offset_y = pos_1
 
-            if pos_2 >= pz / 2: offset_z = pz / 2
-            if pos_2 <= -pz / 2:
-                offset_z = -pz / 2
+            if pos_2 >= self.pz / 2:
+                offset_z = self.pz / 2
+            elif pos_2 <= -self.pz / 2:
+                offset_z = -self.pz / 2
             else:
                 offset_z = pos_2
             gamma = 0
 
         # print(number_wall)
-        cen_mass[name] = [offset_x, offset_y, offset_z]
-        history_args[name] = [number_wall, pos_1, pos_2, pos_3]
+        self.cen_mass[name] = [offset_x, offset_y, offset_z]
+        self.history_args[name] = [number_wall, pos_1, pos_2, pos_3]
 
-        test.filling(name, [alpha, beta, gamma, offset_x, offset_y, offset_z])
+        self.test.filling(name, [alpha, beta, gamma, offset_x, offset_y, offset_z])
 
-    def centre_mass():
+    def centre_mass(self):
         m_x = 0
         m_y = 0
         m_z = 0
         sum_m = 0
-        for name in cen_mass:
-            m_x += cen_mass[name][0] * list_bodies[name][6]
-            m_y += cen_mass[name][1] * list_bodies[name][6]
-            m_z += cen_mass[name][2] * list_bodies[name][6]
-            sum_m += list_bodies[name][6]
+        for name in self.cen_mass:
+            m_x += self.cen_mass[name][0] * self.list_bodies[name][6]
+            m_y += self.cen_mass[name][1] * self.list_bodies[name][6]
+            m_z += self.cen_mass[name][2] * self.list_bodies[name][6]
+            sum_m += self.list_bodies[name][6]
 
         c_x = m_x / sum_m
         c_y = m_y / sum_m
@@ -283,31 +295,37 @@ class Optimization:
 
         result = (c_x ** 2 + c_y ** 2 + c_z) ** 0.5
         # num_iter += 1
-        print(current_body, result)
+        print(self.current_body, result)
         return result
 
-    def goal_function():
+    def goal_function(self):
         # call amount of inter
-        inters = test.chek_intersection()
-        dist = centre_mass()
+        inters = self.test.chek_intersection()
+        dist = self.centre_mass()
         return (inters + 100 * dist)
 
-    def function_for_opt(args):
+    def function_for_opt(self, args):
 
-        change_position(current_body, args[0], args[1], args[2], args[3])
-        np.append(goal, goal_function())
-        return (goal_function())
+        self.change_position(self.current_body, args[0], args[1], args[2], args[3])
+        np.append(self.goal, self.goal_function())
+        return (self.goal_function())
 
-    current_body = ''
+    #
 
-    def my_optimization():
+    def my_optimization(self):
         for i in range(3):
-            for name in list_bodies:
-                current_body = name
-                x0 = history_args[current_body]
-                res = minimize(function_for_opt, x0, method='powell',
+            for name in self.list_bodies:
+                self.current_body = name
+                x0 = self.history_args[self.current_body]
+                res = minimize(self.function_for_opt, x0, method='powell',
                                options={'maxfev': 30})
                 print(res.x)
+
+        self.test.visual_workspace()
+        plt.plot(self.goal)
+        plt.ylabel('Goal')
+        plt.show()
+        print(self.goal_function())
 
     # def my_optimization():
     #     x0 = history_args[current_body].copy()
@@ -318,23 +336,14 @@ class Optimization:
     #     print(res.x)
 
 
-walls_function()
-restricted_area()
-test = Inter(bodies_test, modules)
-for name in walls:
-    test.filling(name, [0, 0, 0, 0, 0, 0])
-for name in bans:
-    test.filling(name, [0, 0, 0, 0, 0, 0])
 
-body_random()
-# test.visual_workspace()
-# print(goal_function())
+if __name__ == '__main__':
+    Opt_1 = Optimization()
+    Opt_1.body_random()
+    # test.visual_workspace()
+    # print(goal_function())
 
-# print(history_args)
-my_optimization()
+    # print(history_args)
+    Opt_1.my_optimization()
 
-test.visual_workspace()
-plt.plot(goal)
-plt.ylabel('Goal')
-plt.show()
-print(goal_function())
+
