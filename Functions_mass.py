a1cd27fccbfd3e6cb7efc37b8d0f4751738edb82
Alexander19970212 <53803031+alexander19970212@users.noreply.github.com>
2020-02-14 13:39:47 +1000

    Finish peeping analyse. required analyse any models.
    Signed-off-by: nokvel <nokvel96@gmail.com>

--- Functions_mass.py
+++ Functions_mass.py
@@ -57,14 +57,23 @@ class Balance_mass:
         self.valume_inter_obj = defaultdict(dict)
 
         # for testing, remove later
+
+        self.test_var = 60
+        self.test_2_var = 0.0000
+
         self.d = 3.1
-        self.px = 220 - self.d / 2
-        self.py = 220 - self.d / 2
+        self.px = 220 - self.d
+        self.py = 220 - self.d
         self.pz = 246
+
+
+        self.peep_factor = 0
+        self.peep_list = {}
 
         self.dimensoins = {}
         self.profiles = {}
         self.valume_inter = {}
+        self.inter_mass = 0
         self.display, self.start_display, self.add_menu, self.add_function_to_menu = init_display()
         for model in modules:
             self.modules[model[2]] = read_step_file(os.path.join(model[0], model[1], model[2]))
@@ -75,10 +84,109 @@ class Balance_mass:
             brepbndlib_Add(self.modules[model], bbox)
             xmin, ymin, zmin, xmax, ymax, zmax = bbox.Get()
             self.dimensoins[model] = [xmin, xmax, ymin, ymax, zmin, zmax]
-            print(model)
+            #print(model)
             self.profiles[model] = [self.glue_solids(self.modules[model])]
 
-        print(self.profiles)
+        #print(self.profiles)
+
+    def max_counter(self, facesS_2):
+        section_edges = list(Topo(facesS_2).edges())
+        #print(len(section_edges))
+
+        if len(section_edges) > 0:
+
+            Wire_c = BRepBuilderAPI_MakeWire()
+            prep_list = []
+            Wire_c.Add(section_edges[0])
+            prep_list.append(section_edges[0])
+            ex = TopExp_Explorer(section_edges[0], TopAbs_VERTEX)
+
+            # no need for a loop since we know for a fact that
+            # the edge has only one start and one end
+            c = ex.Current()
+            cv = topods_Vertex(c)
+            v0 = BRep_Tool_Pnt(cv)
+            ex.Next()
+            c = ex.Current()
+            cv = topods_Vertex(c)
+            v1 = BRep_Tool_Pnt(cv)
+            section_edges.pop(0)
+            flag = 0
+            wires = []
+
+            while len(section_edges) > 0:
+
+                new_list = []
+
+                for edges in section_edges:
+                    # Wire_c.Add(edges)
+                    ex = TopExp_Explorer(edges, TopAbs_VERTEX)
+                    c = ex.Current()
+                    cv = topods_Vertex(c)
+                    End_1 = BRep_Tool_Pnt(cv)
+                    ex.Next()
+                    c = ex.Current()
+                    cv = topods_Vertex(c)
+                    End_2 = BRep_Tool_Pnt(cv)
+
+                    if End_1.X() == v0.X() and End_1.Y() == v0.Y() and End_1.Z() == v0.Z():
+                        Wire_c.Add(edges)
+                        v0 = End_2
+                        flag = 0
+                    elif End_1.X() == v1.X() and End_1.Y() == v1.Y() and End_1.Z() == v1.Z():
+                        Wire_c.Add(edges)
+                        v1 = End_2
+                        flag = 0
+                    elif End_2.X() == v0.X() and End_2.Y() == v0.Y() and End_2.Z() == v0.Z():
+                        Wire_c.Add(edges)
+                        v0 = End_1
+                        flag = 0
+                    elif End_2.X() == v1.X() and End_2.Y() == v1.Y() and End_2.Z() == v1.Z():
+                        Wire_c.Add(edges)
+                        v1 = End_1
+                        flag = 0
+                    else:
+                        new_list.append(edges)
+
+                flag += 1
+                section_edges = new_list
+
+                if flag >= 5:
+                    # print('number_ostalos', len(section_edges))
+                    wires.append(Wire_c.Wire())
+                    Wire_c = BRepBuilderAPI_MakeWire()
+
+                    Wire_c.Add(section_edges[0])
+                    ex = TopExp_Explorer(section_edges[0], TopAbs_VERTEX)
+
+                    # no need for a loop since we know for a fact that
+                    # the edge has only one start and one end
+                    c = ex.Current()
+                    cv = topods_Vertex(c)
+                    v0 = BRep_Tool_Pnt(cv)
+                    ex.Next()
+                    c = ex.Current()
+                    cv = topods_Vertex(c)
+                    v1 = BRep_Tool_Pnt(cv)
+                    section_edges.pop(0)
+                    flag = 0
+
+            wires.append(Wire_c.Wire())
+
+            areas = []
+            props = GProp_GProps()
+
+            for wire in wires:
+                brown_face = BRepBuilderAPI_MakeFace(wire)
+                brown_face = brown_face.Face()
+                # props = GProp_GProps()
+                brepgprop_SurfaceProperties(brown_face, props)
+                areas.append(props.Mass())
+
+            return max(areas)
+
+        else:
+            return 0
 
     def glue_solids(self, S2):
 
@@ -94,98 +202,7 @@ class Balance_mass:
 
         facesS_2 = BRepAlgoAPI_Section(face, S2).Shape()
 
-        section_edges = list(Topo(facesS_2).edges())
-        print(len(section_edges))
-
-        Wire_c = BRepBuilderAPI_MakeWire()
-        prep_list = []
-        Wire_c.Add(section_edges[0])
-        prep_list.append(section_edges[0])
-        ex = TopExp_Explorer(section_edges[0], TopAbs_VERTEX)
-
-        # no need for a loop since we know for a fact that
-        # the edge has only one start and one end
-        c = ex.Current()
-        cv = topods_Vertex(c)
-        v0 = BRep_Tool_Pnt(cv)
-        ex.Next()
-        c = ex.Current()
-        cv = topods_Vertex(c)
-        v1 = BRep_Tool_Pnt(cv)
-        section_edges.pop(0)
-        flag = 0
-        wires = []
-
-        while len(section_edges) > 0:
-
-            new_list = []
-
-            for edges in section_edges:
-                # Wire_c.Add(edges)
-                ex = TopExp_Explorer(edges, TopAbs_VERTEX)
-                c = ex.Current()
-                cv = topods_Vertex(c)
-                End_1 = BRep_Tool_Pnt(cv)
-                ex.Next()
-                c = ex.Current()
-                cv = topods_Vertex(c)
-                End_2 = BRep_Tool_Pnt(cv)
-
-                if End_1.X() == v0.X() and End_1.Y() == v0.Y() and End_1.Z() == v0.Z():
-                    Wire_c.Add(edges)
-                    v0 = End_2
-                    flag = 0
-                elif End_1.X() == v1.X() and End_1.Y() == v1.Y() and End_1.Z() == v1.Z():
-                    Wire_c.Add(edges)
-                    v1 = End_2
-                    flag = 0
-                elif End_2.X() == v0.X() and End_2.Y() == v0.Y() and End_2.Z() == v0.Z():
-                    Wire_c.Add(edges)
-                    v0 = End_1
-                    flag = 0
-                elif End_2.X() == v1.X() and End_2.Y() == v1.Y() and End_2.Z() == v1.Z():
-                    Wire_c.Add(edges)
-                    v1 = End_1
-                    flag = 0
-                else:
-                    new_list.append(edges)
-
-            flag += 1
-            section_edges = new_list
-
-            if flag >= 5:
-                # print('number_ostalos', len(section_edges))
-                wires.append(Wire_c.Wire())
-                Wire_c = BRepBuilderAPI_MakeWire()
-
-                Wire_c.Add(section_edges[0])
-                ex = TopExp_Explorer(section_edges[0], TopAbs_VERTEX)
-
-                # no need for a loop since we know for a fact that
-                # the edge has only one start and one end
-                c = ex.Current()
-                cv = topods_Vertex(c)
-                v0 = BRep_Tool_Pnt(cv)
-                ex.Next()
-                c = ex.Current()
-                cv = topods_Vertex(c)
-                v1 = BRep_Tool_Pnt(cv)
-                section_edges.pop(0)
-                flag = 0
-
-        wires.append(Wire_c.Wire())
-
-        areas = []
-        props = GProp_GProps()
-
-        for wire in wires:
-            brown_face = BRepBuilderAPI_MakeFace(wire)
-            brown_face = brown_face.Face()
-            # props = GProp_GProps()
-            brepgprop_SurfaceProperties(brown_face, props)
-            areas.append(props.Mass())
-
-        return max(areas)
+        return self.max_counter(facesS_2)
 
     def move_frame(self):
         self.frame = read_step_file(os.path.join('part_of_sattelate', 'karkase', self.name_frame))
@@ -193,14 +210,18 @@ class Balance_mass:
 
     def change_position1(self, name, number_wall, pos_1, pos_2, pos_3):
         number_wall = int(number_wall % 7)
+        flag = ''
 
         alpha, beta, gamma, offset_x, offset_y, offset_z = 0, 0, 0, 0, 0, 0
 
-        pos_3 = int(pos_3 // 3) * 90
+        libra = int(pos_3 // 3) * 90
+        pos_3 = 0
         if number_wall == 0:
             alpha = 90
-            offset_y = self.py / 2 - self.d / 2 + self.dimensoins[name][4]
+
+            offset_y = self.py / 2 - self.d / 2 + self.dimensoins[name][4]+self.test_2_var
             beta = pos_3
+            flag = 'xyz'
             if pos_1 >= self.px / 2:
                 offset_x = self.px / 2
             elif pos_1 <= -self.px / 2:
@@ -218,8 +239,9 @@ class Balance_mass:
 
         if number_wall == 1:
             alpha = -90
-            offset_y = self.py / 2 + self.d / 2 - self.dimensoins[name][4]
+            offset_y = self.py / 2 + self.d / 2 - self.dimensoins[name][4]-self.test_2_var
             beta = pos_3
+            flag = 'xyz'
             if pos_1 >= self.px / 2:
                 offset_x = self.px / 2
             elif pos_1 <= -self.px / 2:
@@ -237,8 +259,9 @@ class Balance_mass:
 
         if number_wall == 2:
             beta = -90
-            offset_x = self.px / 2 - self.d / 2 + self.dimensoins[name][4]
+            offset_x = self.px / 2 - self.d / 2 + self.dimensoins[name][4]+self.test_2_var
             alpha = pos_3
+            flag = 'yxz'
             if pos_1 >= self.py / 2:
                 offset_y = self.py / 2
             elif pos_1 <= -self.py / 2:
@@ -256,8 +279,9 @@ class Balance_mass:
 
         if number_wall == 3:
             beta = 90
-            offset_x = self.px / 2 + self.d / 2 - self.dimensoins[name][4]
+            offset_x = self.px / 2 + self.d / 2 - self.dimensoins[name][4]-self.test_2_var
             alpha = pos_3
+            flag = 'yxz'
             if pos_1 >= self.py / 2:
                 offset_y = self.py / 2
             elif pos_1 <= -self.py / 2:
@@ -275,8 +299,9 @@ class Balance_mass:
 
         if number_wall == 4:
             alpha = -90
-            offset_y = -(self.py / 2 - self.d / 2 + self.dimensoins[name][4])
+            offset_y = -(self.py / 2 - self.d / 2 + self.dimensoins[name][4]+self.test_2_var)
             beta = pos_3
+            flag = 'xyz'
             if pos_1 >= self.px / 2:
                 offset_x = self.px / 2
             elif pos_1 <= -self.px / 2:
@@ -294,8 +319,9 @@ class Balance_mass:
 
         if number_wall == 5:
             alpha = 90
-            offset_y = -(self.py / 2 + self.d / 2 - self.dimensoins[name][4])
+            offset_y = -(self.py / 2 + self.d / 2 - self.dimensoins[name][4]-self.test_2_var)
             beta = pos_3
+            flag = 'xyz'
             if pos_1 >= self.px / 2:
                 offset_x = self.px / 2
             elif pos_1 <= -self.px / 2:
@@ -313,8 +339,9 @@ class Balance_mass:
 
         if number_wall == 6:
             beta = 90
-            offset_x = -(self.px / 2 - self.d / 2 + self.dimensoins[name][4])
+            offset_x = -(self.px / 2 - self.d / 2 + self.dimensoins[name][4]+self.test_2_var)
             alpha = pos_3
+            flag = 'yxz'
             if pos_1 >= self.py / 2:
                 offset_y = self.py / 2
             elif pos_1 <= -self.py / 2:
@@ -332,8 +359,9 @@ class Balance_mass:
 
         if number_wall == 7:
             beta = -90
-            offset_x = -(self.px / 2 + self.d / 2 - self.dimensoins[name][4])
+            offset_x = -(self.px / 2 + self.d / 2 - self.dimensoins[name][4]-self.test_2_var)
             alpha = pos_3
+            flag = 'yxz'
             if pos_1 >= self.py / 2:
                 offset_y = self.py / 2
             elif pos_1 <= -self.py / 2:
@@ -349,12 +377,43 @@ class Balance_mass:
                 offset_z = pos_2
             gamma = 0
 
-        self.filling(name, [alpha, beta, gamma, offset_x, offset_y, offset_z])
+        if number_wall in [0, 1, 4, 5]:
+            alpha_2, beta_2, gamma_2 = 0, libra, 0
+
+        elif number_wall in [2, 3, 6, 7]:
+            alpha_2, beta_2, gamma_2 = libra, 0, 0
+
+
+        #print(number_wall)
+        #flag = 'xyz'
+
+        self.filling(name, [alpha, beta, gamma, offset_x, offset_y, offset_z], [alpha_2, beta_2, gamma_2])
+
+    def peeping_all_frame(self):
+        self.peep_factor = 0
+        for model in self.modules:
+            peep = self.peeping_frame(model)
+            if peep == 0: print(model, 'dont touch')
+            else:
+                proc_peep = peep/self.profiles[model][0]
+                if proc_peep < 0.98:
+                    self.peep_list[model] = proc_peep
+                    self.peep_factor += 1-proc_peep
+                    print(model, 1-proc_peep)
+                #else: print(model, 1)
+
+
+    def peeping_frame(self, model):
+
+        facesS_2 = BRepAlgoAPI_Section(self.frame, self.modules[model]).Shape()
+
+        return self.max_counter(facesS_2)
+
 
     def change_object(self):
         pass
 
-    def filling(self, name_body, coord_centres):
+    def filling(self, name_body, coord_centres, coord_centres_2):
         '''
 
         :param name_body:
@@ -366,11 +425,22 @@ class Balance_mass:
         5 - смещенмие по z
         :return:
         '''
-        print(name_body)
+        #print(name_body, coord_centres)
         shape = self.modules[name_body]
         # print(body)
 
         r = R.from_euler('xyz', [coord_centres[0], coord_centres[1], coord_centres[2]], degrees=True)
+        Rot = r.as_dcm()
+
+        trsf = gp_Trsf()
+        Mat = gp_Mat(Rot[0][0], Rot[0][1], Rot[0][2],
+                     Rot[1][0], Rot[1][1], Rot[1][2],
+                     Rot[2][0], Rot[2][1], Rot[2][2])
+
+        trsf.SetRotation(gp_Quaternion(Mat))
+        shape.Move(TopLoc_Location(trsf))
+
+        r = R.from_euler('xyz', [coord_centres_2[0], coord_centres_2[1], coord_centres_2[2]], degrees=True)
         Rot = r.as_dcm()
 
         trsf = gp_Trsf()
@@ -387,20 +457,21 @@ class Balance_mass:
         shape.Move(TopLoc_Location(trsf))
 
     def inter_with_frame(self):
-        print('Start_inter_analyse')
+        #print('Start_inter_analyse')
         props = GProp_GProps()
         for model in self.modules:
-            print('#')
+            #print('#')
             body_inter = BRepAlgoAPI_Common(self.frame, self.modules[model]).Shape()
             self.display.DisplayShape(body_inter, color='YELLOW')
             brepgprop_VolumeProperties(body_inter, props)
             self.valume_inter[model] = props.Mass()
 
-        print(self.valume_inter)
+        #print(self.valume_inter)
 
     def inter_objects(self):
 
-        print('Start_inter_analyse')
+        #print('Start_inter_analyse')
+        self.inter_mass = 0
         props = GProp_GProps()
 
         for i in range(len(self.names_models) - 2):
@@ -412,9 +483,11 @@ class Balance_mass:
                 mass = props.Mass()
                 #print(mass)
                 if mass > 0:
+                    self.inter_mass += mass
                     self.valume_inter_obj[self.names_models[i]][self.names_models[j]] = mass
 
-        print(self.valume_inter_obj)
+        #print(self.valume_inter_obj)
+        #print(self.inter_mass)
 
     def centre_mass(self):
         pass
@@ -448,30 +521,31 @@ class Balance_mass:
 
     def body_random(self):
         for name in self.modules:
-            pos_1 = random.uniform(max(self.px, self.py) / 2, -max(self.px, self.py) / 2)
-            pos_2 = random.uniform(self.pz / 2, -self.pz / 2)
+            pos_1 = random.uniform(max(self.px, self.py) / 2 - self.test_var, self.test_var-max(self.px, self.py) / 2)
+            pos_2 = random.uniform(self.pz / 2 - self.test_var, self.test_var-self.pz / 2)
             pos_3 = random.uniform(0, 4)
-            Number_wall = random.uniform(0, 9)
+            Number_wall = random.uniform(0, 20)
 
             self.change_position1(name, Number_wall, pos_1, pos_2, pos_3)
 
 
 if __name__ == '__main__':
     frame = ['part_of_sattelate', 'karkase', 'Assemb.STEP']
-    modules = [['part_of_sattelate', 'pribore', 'Camara_WS16.STEP'],
+    modules = [['part_of_sattelate', 'pribore', 'Camara2_WS16.STEP'],
                ['part_of_sattelate', 'pribore', 'DAV_WS16.STEP'],
-               ['part_of_sattelate', 'pribore', 'All_SEP_WS16.STEP'],
+               #['part_of_sattelate', 'pribore', 'All_SEP_WS16.STEP'],
                ['part_of_sattelate', 'pribore', 'Magnitometr.STEP'],
-               # ['part_of_sattelate', 'pribore', 'Mahovik_WS16.STEP'],
+               #['part_of_sattelate', 'pribore', 'Mahovik_WS16.STEP'],
                ['part_of_sattelate', 'pribore', 'Radio_WS16.STEP'],
                ['part_of_sattelate', 'pribore', 'Solar_battery_WS16.STEP'],
                ['part_of_sattelate', 'pribore', 'UKV.STEP'],
-               #['part_of_sattelate', 'pribore', 'DAV_WS16.STEP'],
+               ['part_of_sattelate', 'pribore', 'DAV_WS16.STEP'],
                ['part_of_sattelate', 'pribore', 'Vch_translator_WS16.STEP']]
     test = Balance_mass('Assemb.STEP', modules)
     test.body_random()
     test.move_frame()
-    #test.inter_with_frame()
-    test.inter_objects()
+    test.inter_with_frame()
+    test.peeping_all_frame()
+    #test.inter_objects()
     test.vizualization_all()
     test.move_frame()
